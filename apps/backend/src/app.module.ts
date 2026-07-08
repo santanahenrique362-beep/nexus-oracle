77c56084210f6fd272be488f97d363e82b86dbdb
santanahenrique362-beep <santanahenrique362@gmail.com>
2026-07-08 19:49:52 -0300

    Add MarketModule to AppModule imports

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OracleController } from './oracle.controller';
 import { OracleService } from './oracle.service';
 import { PrismaModule } from './database/prisma.module';
+import { MarketModule } from './market/market.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MarketModule],
   controllers: [OracleController],
   providers: [OracleService],
 })
