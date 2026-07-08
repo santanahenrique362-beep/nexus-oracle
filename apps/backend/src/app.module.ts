171b67b1ff2a8377f053d47c31d90d6eecc680cf
santanahenrique362-beep <santanahenrique362@gmail.com>
2026-07-08 14:24:22 -0300

    Update app.module.ts

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { OracleController } from './oracle.controller';
+import { OracleService } from './oracle.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [],
+  controllers: [AppController, OracleController],
+  providers: [OracleService],
 })
 export class AppModule {}
