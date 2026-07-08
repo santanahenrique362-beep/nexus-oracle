300158314eb97e5d17ce2022b04b3bcd36cb0648
santanahenrique362-beep <santanahenrique362@gmail.com>
2026-07-08 19:16:48 -0300

    Update app.module.ts

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { OracleController } from './oracle.controller';
 import { OracleService } from './oracle.service';
+import { PrismaModule } from './database/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, OracleController],
+  imports: [PrismaModule],
+  controllers: [OracleController],
   providers: [OracleService],
 })
 export class AppModule {}
